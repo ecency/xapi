bbe353de5ccec8f12a84812ca16ee5de0134b35c
feruz <fmiboy@gmail.com>
2020-04-29 14:21:08 +0300

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# REST API
+
+## Usage: 
+
+You can use Hive blockchain API methods this way:
+
+https://xapi.esteem.app/getAccounts?names[]=ned  
+https://xapi.esteem.app/getState?path=/trending  
+https://xapi.esteem.app/getDiscussionsByHot?query={"tag":"photography","limit":"10"}  
+
+## API UI 
+
+Test REST API on Swagger API UI here: 
+
+https://hive.hivesigner.com
---
+++ routes/api.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const decamelize = require('decamelize');
+const _ = require('lodash');
+const hive = require('@hiveio/hive-js');
+const methodsMap = require('@hiveio/hive-js/lib/api/methods.js');
+
+const methods = methodsMap.default;
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.redirect('https://hivesigner.com');
+});
+
+router.post('/rpc', (req, res) => {
+  const { method, params, id } = req.body;
+  const mapping = _.filter(methods, { method: method });
+  hive.api.send(mapping[0].api, {
+    method: method,
+    params: params,
+  }, (err, result) => {
+    res.send({
+      jsonrpc: '2.0',
+      id,
+      method,
+      result,
+    });
+  });
+});
+
+router.get('/:method', (req, res) => {
+  const query = parseQuery(req.query);
+  const method = decamelize(req.params.method, '_');
+  const mapping = _.filter(methods, { method: method });
+  let params = [];
+  if (mapping[0].params) {
+    mapping[0].params.forEach((param) => {
+      const queryParam = query[param] || query[decamelize(param)];
+      params.push(queryParam);
+    });
+  }
+  hive.api.send(mapping[0].api, {
+    method: method,
+    params: params
+  }, (err, result) => {
+    const json = query.scope
+      ? result[query.scope] : result;
+    res.json(json);
+  });
+});
+
+const parseQuery = (query) => {
+  let newQuery = {};
+  Object.keys(query).map(key => {
+    let value = query[key];
+    try { value = JSON.parse(decodeURIComponent(value)); }
+    catch (e) { }
+    newQuery[key] = value;
+  });
+  return newQuery;
+};
+
+module.exports = router;
