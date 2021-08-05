27cd40709776628f0d7046c1ef9b02fa9e7fa1c5
feruz <fmiboy@gmail.com>
2021-08-05 23:41:04 +0300

    server change

--- routes/utils.js
+++ routes/utils.js
@@ -1,6 +1,6 @@
 const SERVER_LIST = [
-  'https://api.hive.blog',
   'https://rpc.ecency.com',
+  'https://api.hive.blog',
   'https://api.deathwing.me',
   'https://anyx.io',
   'https://rpc.ausbit.dev',
